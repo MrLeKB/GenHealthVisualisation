6f15343bdf230737339f594fee0a66043a14b522
Li Yi Fan <liyifan28@gmail.com>
2022-10-21 16:54:59 +0800

    test

--- app/vis_copy.py
+++ app/vis_copy.py
@@ -5,10 +5,5 @@ app2 = Flask(__name__)
 @app2.route("/")
 def index():
      print("second thread______________________________________________")
-     return render_template("FinalVisualisation.html")
-
-@app2.route("/twitter_aug")
-def test():
-     return render_template("twitter_aug.html")
 
 
