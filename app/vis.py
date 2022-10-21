fe4adb59f104d27e6771f3218f3e68b2763365e6
Li Yi Fan <liyifan28@gmail.com>
2022-10-21 17:04:31 +0800

    test thread

--- app/vis.py
+++ app/vis.py
@@ -8,8 +8,8 @@ def index():
      print("first thread______________________________________________")
      return render_template("FinalVisualisation.html")
 
-@app.route("/twitter_aug")
-def test():
-     return render_template("twitter_aug.html")
+# @app.route("/twitter_aug")
+# def test():
+#      return render_template("twitter_aug.html")
 
 
--- wsgi.py
+++ wsgi.py
@@ -1,23 +1,23 @@
 import threading
 from app.vis import app
-from app.vis_copy import app2
+#from app.vis_copy import app2
 
 def visualisation():
     app.run()
 
 def test():
     print ("test______________________________________test____________")
-    app2.run()
+    #app2.run()
 
 if __name__ == "__main__":
     threads = list()
     vis_thread= threading.Thread(target=visualisation)
-    threads.append(t)
+    test_thread= threading.Thread(target=test)
+
+    threads.append(vis_thread)
     print('Starting Thread {}'.format(i))
     vis_thread.start()
 
-    vis_thread2= threading.Thread(target=test)
-
-    threads.append(t)
+    threads.append(test_thread)
     print('Starting Thread {}'.format(i))
-    vis_thread2.start()
+    test_thread.start()
