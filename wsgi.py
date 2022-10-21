5973b235dd11431b7b54dde60ccb33c2e86ac917
Li Yi Fan <liyifan28@gmail.com>
2022-10-21 16:29:19 +0800

    thread testing

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,13 @@
-from app.main import app
+import threading
+from app.vis import vis
 
+
+def visualisation():
+    vis.run()
+    
 if __name__ == "__main__":
-    app.run()
+    threads = list()
+    vis_thread= threading.Thread(target=visualisation)
+    threads.append(t)
+    print('Starting Thread {}'.format(i))
+    vis_thread.start()
