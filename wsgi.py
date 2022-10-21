61c681afc74041eca6ac93a24b5f1f9d1f569cc8
Li Yi Fan <liyifan28@gmail.com>
2022-10-21 17:23:09 +0800

    test

--- wsgi.py
+++ wsgi.py
@@ -1,23 +1,46 @@
+# import threading
+# from app.vis import app
+# #from app.vis_copy import app2
+
+# def visualisation():
+#     app.run()
+
+# def test(sad):
+#     print ("test______________________________________test____________")
+#     #app2.run()
+
+# if __name__ == "__main__":
+#     threads = list()
+#     vis_thread= threading.Thread(target=visualisation)
+#     test_thread= threading.Thread(target=test)
+
+#     print('Starting Thread 1')
+#     vis_thread.start()
+#     print('Starting Thread 2')
+#     test_thread.start()
+
+#     vis_thread.join()
+#     test_thread.join()
+
+
+import random
 import threading
-from app.vis import app
-#from app.vis_copy import app2
+import time
 
-def visualisation():
-    app.run()
+def sleeper():
+    time.sleep(random.randrange(1, 20))
 
-def test():
-    print ("test______________________________________test____________")
-    #app2.run()
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    # create and start our threads
     threads = list()
-    vis_thread= threading.Thread(target=visualisation)
-    test_thread= threading.Thread(target=test)
+    for i in range(4):
+        t = threading.Thread(target=sleeper) # pass in the callable
+        threads.append(t)
+        print('Starting Thread {}'.format(i))
+        t.start()
 
-    threads.append(vis_thread)
-    print('Starting Thread 1')
-    vis_thread.start()
+    # wait for each to finish (join)
+    for i, t in enumerate(threads):
+        t.join()
+        print('Thread {} Stopped'.format(i))
 
-    threads.append(test_thread)
-    print('Starting Thread 2')
-    test_thread.start()
