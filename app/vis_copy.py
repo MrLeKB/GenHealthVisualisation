2450f190fe1b3e589bc8e4a394d7f81a06c909a6
Li Yi Fan <liyifan28@gmail.com>
2022-10-21 16:42:03 +0800

    test thread

--- app/vis_copy.py
+++ app/vis_copy.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 
 app2 = Flask(__name__)
 
-@app.route("/")
+@app2.route("/")
 def index():
      return render_template("FinalVisualisation.html")
 
-@app.route("/twitter_aug")
+@app2.route("/twitter_aug")
 def test():
      return render_template("twitter_aug.html")
 
