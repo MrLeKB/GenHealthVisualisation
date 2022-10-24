b695ba909d79115ce264eed8ad14d36710720b12
Li Yi Fan <liyifan28@gmail.com>
2022-10-24 12:01:20 +0800

    test

--- app/app.py
+++ app/app.py
@@ -29,4 +29,5 @@ def initialise_analysis():
      return None
 
      
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- wsgi.py
+++ wsgi.py
@@ -1,31 +1,4 @@
-from app.vis import app
-# #from app.vis_copy import app2
-
-# def visualisation():
-#     print("first thread______________________________________________")
-#     #app.run()
-
-# def test():
-#       return "<h1>Welcome to CodingX</h1>"
-
-
-# if __name__ == "__main__":
-    # test
-    #visualisation()
-    # test()
-    # visualisation()
-    # test()
-    # threads = list()
-    # vis_thread= threading.Thread(target=visualisation)
-    # test_thread= threading.Thread(target=test)
-
-    # print('Starting Thread 1')
-    # vis_thread.start()
-    # print('Starting Thread 2')
-    # test_thread.start()
-
-    # vis_thread.join()
-    # test_thread.join()
+from app.app import app
 
 
 
